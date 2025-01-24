b162d1f3d247cd1198cd3184c836b437c4266a0a
arjaybudzz <archessonan.1@gmail.com>
2025-01-24 14:31:20 +0800

    Answered Chapter 2 exercise 2

---
+++ Chapter2/exercise2.c
@@ -0,0 +1,28 @@
+
+/*
+	2. Consider the following program:
+
+	#include <stdio.h>
+
+	int main(void)
+	{
+		printf("Parkinson's Law:\nWork expands so as to ");
+		printf("fill the time\n");
+		printf("available for its completion.\n");
+		return 0;
+	}
+
+	(a) Identify the directives and statements in this program.
+	(b) What output does the program produce?
+
+	ANSWER:
+
+	(a) The directive is the #include preprocessor and the statements are printf() and return.
+	(b) The output of the program is:
+
+	Parkinson's Law:
+	Work expands so as to fill the time
+	available for its completion.	
+*/
+
+
