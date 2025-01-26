eecf24beede13f76570951ce7a9627fa1125b2ea
arjaybudzz <archessonan.1@gmail.com>
2025-01-26 18:24:10 +0800

    Answered Chapter 2 exercise 4

---
+++ Chapter2/exercise4.c
@@ -0,0 +1,32 @@
+/*
+ * Write a program that declares several int and float variables—without initializing
+ * them—and then prints their values. Is there any pattern to the values? (Usually there isn’t.)
+ * Answer:
+ *
+ * No. There is no pattern to the values.
+ */
+
+#include <stdio.h>
+
+int main() {
+	int i;
+	int j;
+	int k;
+	int l;
+	float m;
+	float n;
+	float o;
+	float p;
+
+	printf("Value of int i:		%d\n", i);
+	printf("Value of int j:		%d\n", j);
+	printf("Value of int k:		%d\n", k);
+	printf("Value of int l:		%d\n", l);
+	printf("Value of float m:	%f\n", m);
+	printf("Value of float n:	%f\n", n);
+	printf("Value of float o:	%f\n", o);
+	printf("Value of float p:	%f\n", p);
+
+
+	return 0;
+}
